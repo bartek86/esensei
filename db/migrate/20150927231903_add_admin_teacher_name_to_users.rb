06bcd0cbd5225bda2f085005a002bf4f075ed7fa
morihito <bartek.lesniak86@gmail.com>
2015-09-27 23:55:10 +0000

    Added devise and User model
    Devise authentication is available. User model got additional attributes: admin, teacher, name.
    Devise views changed to support new attr. Application controler got new parameter sanitazer for devise controllers.
    navbar got conditional log in/out button.

---
+++ db/migrate/20150927231903_add_admin_teacher_name_to_users.rb
@@ -0,0 +1,7 @@
+class AddAdminTeacherNameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :teacher, :boolean, default: false, null: false
+    add_column :users, :admin, :boolean, default: false, null: false
+  end
+end
