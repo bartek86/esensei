1fd6f8000b77443e16df684c1e72380a897d80df
morihito <bartek.lesniak86@gmail.com>
2015-10-18 12:31:40 +0000

    added lorem seeds for main resources

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'haml', '~> 4.0.6'
 gem 'simple_form', '~> 3.1.0'
 gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'devise', '~> 3.5.1'
+gem 'faker'
 
 group :development, :test do
   gem 'byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
 
 User.create!(   name: "Admin",
                 email: "admin@example.com",
                 password: "admin123",
                 password_confirmation: "admin123",
                 admin: true)
+                
+10.times do
+    Topic.create(
+        name: Faker::Lorem.word,
+        theory: Faker::Lorem.paragraphs
+    )
+end
+
+100.times do
+    Problem.create(
+        body: Faker::Lorem.sentence,
+        true_answer: Faker::Lorem.word,
+        answer2: Faker::Lorem.word,
+        answer3: Faker::Lorem.word,
+        answer4: Faker::Lorem.word,
+        topic_id: rand(1..10)
+    )
+end
+
+100.times do
+    Sign.create(
+        name: Faker::Lorem.characters(1),
+        onyomi: Faker::Lorem.word,
+        kunyomi: Faker::Lorem.word,
+        translation: Faker::Lorem.word,
+        sign_type: "kanji")
+end
+
+46.times do
+    Sign.create(
+        name: Faker::Lorem.characters(1),
+        onyomi: " - ",
+        kunyomi: Faker::Lorem.word,
+        translation: Faker::Lorem.word,
+        sign_type: "hiragana")
+end    
